8b97cd76c6ec5a65c01d713e103ed1c538229bc2
Aryan Patil <84935470+aaaryan17@users.noreply.github.com>
2022-11-07 01:43:06 +0530

    Add files via upload

---
+++ admin_login.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,'LearnEd');
+
+// Check connection
+
+
+$email = $_POST['email'];  
+$password = $_POST['Password'];  
+      
+        //to prevent from mysqli injection  
+   
+        
+        $sql = "SELECT * from admin where email = '$email' and Password = '$password'";  
+        
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+       
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+            header("Location:admin.html");
+            exit();
+        }  
+        else{  
+            echo "<h3><center>Invalid username or password </center></h3>";
+        }   
+$conn->close();  
+?>
